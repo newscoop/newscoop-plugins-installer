405bc80b0496d65eb247ff21b628794ab9774462
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2013-02-25 14:22:53 +0100

    add avaiable plugins cache

--- PluginsInstaller.php
+++ PluginsInstaller.php
@@ -29,4 +29,40 @@ class PluginsInstaller extends LibraryInstaller
     {
         return 'newscoop-plugin' === $packageType;
     }
+
+
+    /**
+    * {@inheritDoc}
+    */
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        parent::install($repo, $package);
+
+        $this->findAllPlugins();
+    }
+
+    public function findAllPlugins()
+    {
+        $plugins = array();
+        $finder = new Finder();
+        $pluginsDirectory = $this->vendorDir. '../plugins';
+        $cacheDirectory = $this->vendorDir. '../cache';
+        $elements = $finder->depth('== 0')->in($pluginsDirectory);
+        foreach ($elements as $element) {
+            if (count($elements) > 0) {
+                $vendorName = $element->getFileName();
+                $secondFinder = new Finder();
+                $directories = $secondFinder->depth('== 0')->in($element->getPathName());
+                foreach ($directories as $directory) {
+                    $pluginName = $directory->getFileName();
+                    $className = $vendorName . '\\' .$pluginName . '\\' . $vendorName . $pluginName;
+                    if (class_exists($className)) {
+                        $plugins[] = $className;
+                    }
+                }
+            }
+        }
+
+        file_put_contents($cacheDirectory.'/avaiable_plugins.json', json_encode($plugins));
+    }
 }
